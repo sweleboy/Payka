893e1b09669cef19d44ba3e7a114038726ab9bf7
Илья Гниденко <gn.ilia@yandex.ru>
2025-08-28 22:44:51 +0500

    add user properties

--- Payka.Domain/Models/User.cs
+++ Payka.Domain/Models/User.cs
@@ -2,11 +2,50 @@
 
 namespace Payka.Domain.Models
 {
+	/// <summary>
+	/// Представляет модель пользователя.
+	/// </summary>
 	public class User : DomainModelBase
 	{
-		private User()
+		/// <summary>
+		/// Представляет ник-нейм пользователя (логин).
+		/// </summary>
+		public string UserName
 		{
+			get;
+			set;
+		} = string.Empty;
 
+		/// <summary>
+		/// Представляет полное имя пользователя (ФИО).
+		/// </summary>
+		public string FullName
+		{
+			get;
+			set;
+		} = string.Empty;
+
+		private User(Guid id, string userName, string fullName)
+		{
+			Id = id;
+			UserName = userName;
+			FullName = fullName;
+		}
+
+		/// <summary>
+		/// Создаёт пользователя.
+		/// </summary>
+		/// <param name="id">Идентификатор пользователя.</param>
+		/// <param name="userName">Ник-нейм пользователя (логин).</param>
+		/// <param name="fullName">полное имя пользователя (ФИО).</param>
+		/// <returns>Пользователь.</returns>
+		public static User Create(Guid id, string userName, string fullName)
+		{
+			return new User(
+				id,
+				userName,
+				fullName
+			);
 		}
 	}
 }
