4ac78c16a0f6a91bdef8a3da75ab42058ec266ba
Илья Гниденко <gn.ilia@yandex.ru>
2025-09-02 18:54:32 +0500

    install npgsql

--- Payka.API/Extensions/DependencyExtensions.cs
+++ Payka.API/Extensions/DependencyExtensions.cs
@@ -1,13 +1,14 @@
-﻿namespace Payka.API.Extensions
+﻿using Microsoft.EntityFrameworkCore;
+using Payka.Dal;
+
+namespace Payka.API.Extensions
 {
 	public static class DependencyExtensions
 	{
 		public static IServiceCollection AddPaykaServices(this IServiceCollection services, string connectionString)
 		{
-			//services.AddDbContext<ApplicationDbContext>(options =>
-			//	options.UseSqlServer(connectionString), ServiceLifetime.Transient);
-			//services.AddDbContext<InitializedDbContext>(options =>
-			//	options.UseSqlServer(connectionString));
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseNpgsql(connectionString), ServiceLifetime.Transient);
 
 			return services;
 		}
